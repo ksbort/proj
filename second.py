0ed49926aebe91f89b4cbe61b838cf30d88568ef
Javier Tan <javiertan43@gmail.com>
2017-12-24 19:59:34 +0800

    save1

--- second.py
+++ second.py
@@ -1,17 +1,31 @@
 import time
+#one after dialogue and two after speech
+def one():
+	time.sleep(1)
+def two():
+	time.sleep(2)
 #Delimiter
 print('\n------------------------')
 time.sleep(.1)
 print('\n*DING DONG DING DONG*')
-time.sleep(1)
+one()
 print('\n\"..........\"')
-time.sleep(2)
+two()
 print('\n\"uuuuurrrrrrrrrggg....\"')
-time.sleep(2)
+two()
 print('\n[You wake up from the sound of an alarm bell.]')
-time.sleep(1)
+one()
 print('\n[You look around and find yourself in a dark room.]')
-time.sleep(1)
-print('\"Where am I? What is this place? What am I doing here?\"')
-time.sleep(2)
-print('[You feel your')
+one()
+print('\n\"Where am I? What is this place? What am I doing here?\"')
+two()
+print('\n[You feel your limbs bound by leather to the bed that you are lying on...')
+one()
+print('\n...and an IV tube sticking out from your left hand.]')
+two() #impact
+print('\n[You struggle a little and manage to free yourself from the worn out straps.]')
+one()
+print('\n\"Just how long have i been unconscious for?...\"')
+two()
+print('\n[You look around again, trying to find an answer to the many questions on your mind]')
+one() #Overflow on above line on 80 long screens
